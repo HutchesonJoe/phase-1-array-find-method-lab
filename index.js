0ed0c996b2142c9f4f6f132bc59c220710383a8d
Joe <hutcheson2001@yahoo.com>
2022-01-17 13:49:58 -0500

    commit tree 124818599d334c792640f17d1e670379e1a4880a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+function superbowlWin(array){
+   let results = array.find(item => item.result === "W")
+ 
+   if (results){
+   return results.year
+   }}
+   
+ 
+ //superbowlWin(record)
+  
+  
+  
